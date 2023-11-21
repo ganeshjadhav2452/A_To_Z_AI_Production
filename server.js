8f2ebe78995fceb9927b6a6184bd48c5a5391280
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-21 21:23:09 +0530

    lsdjf

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(errorHandler)
 
-//deployment code 
+//deployment code for sending frontend
 
 app.use(express.static(path.join(__dirname, './client/build')))
 
