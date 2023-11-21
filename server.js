78e2d0ce017c295a062d105079b9176e3f5a1894
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-21 21:15:12 +0530

    lsdjf

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(errorHandler)
 app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 
