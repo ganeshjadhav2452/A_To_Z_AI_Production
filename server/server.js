67477af469fd6cb2e165bf74a5ec474bceb934fa
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 22:17:29 +0530

    sdjf

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const app = express()
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morgan('common'))
 
 // REST API Routes
 app.use('/api/v1/auth/', authRoutes)
