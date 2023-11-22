7c072817930102aecc95456bd3b9094d908f8dde
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 11:16:47 +0530

    lsdjf

--- server/server.js
+++ server/server.js
@@ -20,26 +20,26 @@ const corsOptions = {
 
 //intilizing app
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(errorHandler)
-app.use(cors(corsOptions))
 
 //deployment code for sending frontend
 
-app.use('*', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+// app.use('*', (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-    next()
-})
+//     next()
+// })
 
-app.use('*', (req, res) => {
+// app.use('*', (req, res) => {
 
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'))
+// })
 
 
 // REST API Routes
