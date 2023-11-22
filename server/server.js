663d31e2c00f09a7b3617989148993e07980af80
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 20:01:03 +0530

    ldjf

--- server/server.js
+++ server/server.js
@@ -25,29 +25,8 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(errorHandler)
 
-// Middleware to handle CORS
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
 
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
 
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
 
 
 // app.use('*', (req, res) => {
