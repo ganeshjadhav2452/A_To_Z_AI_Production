24e58e372e98511cff5f973f613b19f3b6cd590f
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 19:07:40 +0530

    sldjf

--- server/server.js
+++ server/server.js
@@ -25,22 +25,19 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(errorHandler)
 
-app.use('*', async (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    next()
+// Middleware to handle CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-}
-
-
-)
+    if (req.method === 'OPTIONS') {
+        // Handling preflight requests
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 
 
