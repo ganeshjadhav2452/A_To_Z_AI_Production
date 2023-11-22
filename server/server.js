622aaffbe85520bc763f15e0deb042e5988facad
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 19:16:01 +0530

    sldfj

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(morgan('dev'))
 app.use(errorHandler)
 
 // Middleware to handle CORS
-app.use((req, res, next) => {
+app.options((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
